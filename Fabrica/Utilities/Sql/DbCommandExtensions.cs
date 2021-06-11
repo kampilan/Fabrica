70ff5a2979788ad6fe6cadfaeaabea2bbbfc805d
James Moring <james.moring@kampilangroup.com>
2021-06-11 10:40:42 -0400

    Added Sql Utilities

---
+++ Fabrica/Utilities/Sql/DbCommandExtensions.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+using System.Data;
+
+namespace Fabrica.Utilities.Sql
+{
+
+    
+    public static class DbCommandExtensions
+    {
+
+
+        public static void FromSqlRawList( this IDbCommand cmd, string indexed, IEnumerable<object> inputs )
+        {
+
+            var (sql, parameters) = Formatter.FromList( indexed, inputs );
+
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+
+            foreach( var val in parameters )
+            {
+                var param = cmd.CreateParameter();
+                param.Value = val;
+                cmd.Parameters.Add(param);
+            }
+
+
+        }
+
+
+        public static void FromSqlRaw( this IDbCommand cmd, string indexed, params object[] inputs )
+        {
+
+            var (sql, parameters) = Formatter.FromParams( indexed, inputs );
+
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+
+            foreach (var val in parameters )
+            {
+                var param = cmd.CreateParameter();
+                param.Value = val;
+                cmd.Parameters.Add(param);
+            }
+
+        }
+
+
+    }
+
+
+}
---
+++ Fabrica/Utilities/Sql/Formatter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fabrica.Utilities.Sql
+{
+
+
+    public static class Formatter
+    {
+
+        
+        public static (string sql, object[] parameters) FromParams( string indexed, params object[] inputs )
+        {
+
+            var list    = inputs.ToList();
+            var markers = list.Select(o => "?").Cast<object>().ToArray();
+
+            var sql = string.Format(indexed, markers);
+
+            return (sql, inputs);
+
+        }
+
+
+        public static (string sql, object[] parameters) FromList( string indexed, IEnumerable<object> inputs )
+        {
+
+            var list    = inputs.ToList();
+            var markers = list.Select(o => "?").Cast<object>().ToArray();
+
+            var sql = string.Format(indexed, markers);
+
+            return (sql, list.ToArray());
+
+        }
+
+
+    }
+
+
+}
