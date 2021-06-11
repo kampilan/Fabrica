1a83a8372365122324d32837a258b54db2feb6a3
James Moring <james.moring@kampilangroup.com>
2021-06-11 13:18:05 -0400

    Added ProxyTokenEncoder Autofac extensions

---
+++ Fabrica.Api.Support/Identity/Token/AutofacExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Autofac;
+using JetBrains.Annotations;
+
+namespace Fabrica.Api.Support.Identity.Token
+{
+
+    
+    public static  class AutofacExtensions
+    {
+
+
+        public static ContainerBuilder AddProxyTokenEncoder( [NotNull] this ContainerBuilder builder,  [NotNull] string tokenSigningKey )
+        {
+
+            builder.Register(c =>
+                {
+
+                    byte[] key = null;
+                    if (!string.IsNullOrWhiteSpace(tokenSigningKey))
+                        key = Convert.FromBase64String(tokenSigningKey);
+
+                    var comp = new ProxyTokenJwtEncoder
+                    {
+                        TokenSigningKey = key
+                    };
+
+                    return comp;
+
+                })
+                .As<IProxyTokenEncoder>()
+                .SingleInstance();
+
+
+            return builder;
+
+        }
+
+
+    }
+
+
+}
