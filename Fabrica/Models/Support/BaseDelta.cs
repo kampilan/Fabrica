32d9313590580bf3438c136007567e51e75b1409
James Moring <james.moring@kampilangroup.com>
2021-06-17 13:27:40 -0400

    Added BaseDelta

---
+++ Fabrica/Models/Support/BaseDelta.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Fabrica.Models.Support
+{
+
+
+    public class BaseDelta
+    {
+
+
+        [JsonExtensionData]
+        private Dictionary<string,JToken> Overposts { get; } = new ();
+
+        public bool IsOverposted() => Overposts.Count > 0;
+
+        public IEnumerable<string> GetOverpostNames() => Overposts.Keys;
+
+
+    }
+
+
+}
