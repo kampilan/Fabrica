3941543f7033a2d7b2a0653eeca8df29849f3b44
James Moring <james.moring@kampilangroup.com>
2021-06-24 13:12:14 -0400

    Nore Dup checking tweeking

---
+++ Fabrica.Persistence/Persistence/Mediator/Handlers/IDuplicateChecked.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using Fabrica.Models.Support;
+
+namespace Fabrica.Persistence.Mediator.Handlers
+{
+
+    
+    public interface IDuplicateChecked<TResponse> where TResponse: class, IModel
+    {
+
+        void AddDuplicateCheck( Func<TResponse, TResponse, string> template, Func<TResponse, Expression<Func<TResponse, bool>>> predicate );
+
+    }
+
+
+
+}
