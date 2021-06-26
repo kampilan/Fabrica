efa4234ae27ef3df518b97728568fe609ae83efc
James Moring <james.moring@kampilangroup.com>
2021-06-26 11:40:25 -0400

    Added Work Appliance

---
+++ Fabrica.Work/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Fabrica.Work.Appliance;
+
+namespace Fabrica.Work
+{
+
+
+    
+    class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+
+            var bootstrap = new TheBootstrap();
+
+            // *****************************************************************
+            var headless = Console.OpenStandardOutput() == Stream.Null;
+            if (!headless)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Fabrica Work Appliance");
+                Console.WriteLine("The Kampilan Group Inc. (c) 2021");
+                Console.WriteLine("");
+
+            }
+
+            await bootstrap.Run();
+
+        }
+
+    }
+
+}
