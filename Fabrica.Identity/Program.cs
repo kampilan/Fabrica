9e799e7ba2dae6d2b416ec7a81428f27b3a50496
James Moring <james.moring@kampilangroup.com>
2021-06-27 19:50:02 -0400

    Added Fabrica.Identity

---
+++ Fabrica.Identity/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Fabrica.Identity.Appliance;
+
+namespace Fabrica.Identity
+{
+    class Program
+    {
+
+        
+        static async Task Main(string[] args)
+        {
+
+
+            var bootstrap = new TheBootstrap();
+
+            // *****************************************************************
+            var headless = Console.OpenStandardOutput() == Stream.Null;
+            if (!headless)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Fabrica Identity Appliance");
+                Console.WriteLine("The Kampilan Group Inc. (c) 2021");
+                Console.WriteLine("");
+
+            }
+
+            await bootstrap.Run();
+
+        }
+
+    }
+}
