0a25c01a14460697c72add27242f296c3bff0db8
James Moring <james.moring@kampilangroup.com>
2021-06-27 08:06:37 -0400

    Refactor of Topics in Work

---
+++ Fabrica.Work/Topics/ITopicEndpoint.cs
@@ -0,0 +1,16 @@
+﻿namespace Fabrica.Work.Topics
+{
+
+
+    public interface ITopicEndpoint
+    {
+
+        string Topic { get; }
+
+        string Name { get; }
+        string Path { get; }
+
+    }
+
+
+}
---
+++ Fabrica.Work/Topics/ITopicMap.cs
@@ -0,0 +1,14 @@
+﻿namespace Fabrica.Work.Topics
+{
+
+    
+    public interface ITopicMap
+    {
+
+        bool HasTopic( string topic );
+        bool TryGetEndpoint( string topic, out ITopicEndpoint endpoint );
+
+    }
+
+
+}
