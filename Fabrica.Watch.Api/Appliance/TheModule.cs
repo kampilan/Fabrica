9723ba221e1a7bb3aa32eaa2748de510ce6120d0
James Moring <james.moring@kampilangroup.com>
2021-11-03 07:23:57 -0400

    Add Correlation

--- Fabrica.Watch.Api/Appliance/TheModule.cs
+++ Fabrica.Watch.Api/Appliance/TheModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Fabrica.Utilities.Container;
 
 namespace Fabrica.Watch.Api.Appliance
 {
@@ -6,6 +7,16 @@ namespace Fabrica.Watch.Api.Appliance
 
     public class TheModule: Module
     {
+        
+        
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.AddCorrelation();
+
+
+        }
+
     }
 
 
