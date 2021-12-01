bbdba8be030b7681e58e23225ade1ca9323df2aa
James Moring <me@jamesmoring.com>
2021-12-01 13:06:39 -0500

    Fixed CreateMember request

--- Fabrica.Extensions/Persistence/Mediator/CreateMemberEntityRequest.cs
+++ Fabrica.Extensions/Persistence/Mediator/CreateMemberEntityRequest.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Fabrica.Mediator;
 using Fabrica.Models.Support;
+using JetBrains.Annotations;
 using MediatR;
 
 // ReSharper disable UnusedTypeParameter
@@ -20,7 +24,22 @@ public class CreateMemberEntityRequest<TParent,TMember>: ICreateMemberEntityRequ
 
     public string Uid { get; set; } = "";
 
-    public Dictionary<string,object> Delta { get; set; } = new Dictionary<string,object>();
+    public Dictionary<string,object> Delta { get; set; } = new ();
+
+
+    public void FromObject([NotNull] object source)
+    {
+
+        if (source == null) throw new ArgumentNullException(nameof(source));
+
+        foreach (var pi in source.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(p => p.CanRead))
+        {
+            var value = pi.GetValue(source, null);
+            if (value is not null)
+                Delta[pi.Name] = value;
+        }
+
+    }
 
 
 }
