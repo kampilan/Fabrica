b66f9b89bbc51210c6c9e3f91885c3b60bfbc2dc
James Moring <james.moring@kampilangroup.com>
2022-05-17 15:20:55 -0400

    Added logging to AuthHeader Proxy Filter

--- Fabrica.Proxy/Appliance/AuthHeaderProxyConfigFilter.cs
+++ Fabrica.Proxy/Appliance/AuthHeaderProxyConfigFilter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Fabrica.Watch;
 using Yarp.ReverseProxy.Configuration;
 using Yarp.ReverseProxy.Transforms;
 
@@ -17,6 +18,9 @@ namespace Fabrica.Proxy.Appliance
 
         public ValueTask<RouteConfig> ConfigureRouteAsync(RouteConfig route, ClusterConfig cluster, CancellationToken cancel)
         {
+
+            using var logger = this.EnterMethod();
+
 
             route.WithTransformRequestHeaderRemove("Cookies");
             route.WithTransformRequestHeaderRemove("Authorization");
