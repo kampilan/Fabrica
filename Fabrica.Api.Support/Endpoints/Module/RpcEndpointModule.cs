c3b3d680635a9ba97f4e84a188894a2817add47f
James Moring <james.moring@kampilangroup.com>
2023-02-13 10:39:27 -0500

    Add void Response RpcModule

--- Fabrica.Api.Support/Endpoints/Module/RpcEndpointModule.cs
+++ Fabrica.Api.Support/Endpoints/Module/RpcEndpointModule.cs
@@ -12,6 +12,64 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace Fabrica.Api.Support.Endpoints.Module;
 
+
+public abstract class RpcEndpointModule<TRequest> : BaseEndpointModule where TRequest : class, IRequest<Response>
+{
+
+
+    protected RpcEndpointModule(string route, string summary, string description) : base()
+    {
+
+        Route = route;
+        Summary = summary;
+        Description = description;
+
+    }
+
+
+    private string Route { get; }
+    private string Summary { get; }
+    private string Description { get; }
+
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+
+        app.MapPost(Route, async ([AsParameters] RpcHandler handler) => await handler.Handle())
+            .WithMetadata(new SwaggerOperationAttribute { Summary = Summary, Description = Description })
+            .Produces(200)
+            .Produces<ErrorResponseModel>(422);
+
+    }
+
+
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class RpcHandler : BaseMediatorEndpointHandler<TRequest>
+    {
+
+
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+        [FromBody]
+        private TRequest RpcRequest { get; set; } = null!;
+
+
+        protected override Task<TRequest> BuildRequest()
+        {
+
+            using var logger = EnterMethod();
+
+            return Task.FromResult(RpcRequest);
+
+        }
+
+
+    }
+
+
+}
+
+
+
 public abstract class RpcEndpointModule<TRequest,TResponse>: BaseEndpointModule where TRequest : class, IRequest<Response<TResponse>> where TResponse: class
 {
 
