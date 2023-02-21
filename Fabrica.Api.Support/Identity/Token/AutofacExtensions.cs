6b485386414ac9abe90d5b51cc14b5d16c044cc3
James Moring <james.moring@kampilangroup.com>
2023-02-21 10:15:01 -0500

    Aded ProxyAccessTokenSource

--- Fabrica.Api.Support/Identity/Token/AutofacExtensions.cs
+++ Fabrica.Api.Support/Identity/Token/AutofacExtensions.cs
@@ -2,6 +2,7 @@
 // ReSharper disable UnusedMember.Global
 
 using Autofac;
+using Fabrica.Identity;
 
 namespace Fabrica.Api.Support.Identity.Token;
 
@@ -34,6 +35,30 @@ public static  class AutofacExtensions
         return builder;
 
     }
+
+    public static ContainerBuilder AddProxyAccessTokenSource(this ContainerBuilder builder, IClaimSet claims )
+    {
+
+        builder.Register(c =>
+            {
+
+                var encoder = c.Resolve<IProxyTokenEncoder>();
+                var comp = new ProxyAccessTokenSource(encoder, claims);
+                
+                return comp;
+
+            })
+            .AsSelf()
+            .As<IAccessTokenSource>()
+            .SingleInstance();
+
+
+        return builder;
+
+    }
+
+
+
 
 
 }
---
+++ Fabrica.Api.Support/Identity/Token/ProxyAccessTokenSource.cs
@@ -0,0 +1,31 @@
+﻿using Fabrica.Identity;
+using Fabrica.Watch;
+
+namespace Fabrica.Api.Support.Identity.Token;
+
+public class ProxyAccessTokenSource: IAccessTokenSource
+{
+
+    public ProxyAccessTokenSource(IProxyTokenEncoder encoder, IClaimSet claims)
+    {
+        Encoder = encoder;
+        Claims  = claims;
+
+    }
+    
+    private IProxyTokenEncoder Encoder { get; }
+    private IClaimSet Claims { get; }
+
+    public string Name { get; set; } = "";
+    public bool HasExpired { get; set; }
+
+    public Task<string> GetToken()
+    {
+        using var logger = this.EnterMethod();
+
+        var token = Encoder.Encode(Claims);
+        return Task.FromResult(token);
+
+    }
+
+}
