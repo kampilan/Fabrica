0611fce69d257eaf21371e8bb22e2602e8d3390a
James Moring <james.moring@kampilangroup.com>
2023-06-01 20:05:48 -0400

    Chcnged Uid to virtual in BaseReferenceMode

--- Fabrica/Models/Support/BaseReferenceModel.cs
+++ Fabrica/Models/Support/BaseReferenceModel.cs
@@ -6,7 +6,7 @@ public abstract class BaseReferenceModel: IReferenceModel
 {
 
 
-    public abstract string Uid { get; set; }
+    public virtual string Uid { get; set; } = "";
 
 
     private Type GetUnproxiedType()
--- Fabrica/Models/Support/DuplicateCheckBuilder.cs
+++ Fabrica/Models/Support/DuplicateCheckBuilder.cs
@@ -1,49 +1,41 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 using Fabrica.Utilities.Types;
 
-namespace Fabrica.Models.Support
+namespace Fabrica.Models.Support;
+
+public interface IDuplicateCheckBuilder
 {
 
+}
 
-    public interface IDuplicateCheckBuilder
+
+public class DuplicateCheckBuilder<TModel>: IDuplicateCheckBuilder where TModel: class, IModel
+{
+
+        
+    public DuplicateCheckBuilder( Func<TModel,TModel,string> template, Func<TModel,Expression<Func<TModel,bool>>> predicate )
     {
 
-    }
+        Template  = template;
+        Predicate = predicate;
+
+    }        
+        
+
+    private Func<TModel,TModel,string> Template { get; }
+        
+    private Func<TModel,Expression<Func<TModel,bool>>> Predicate { get; }
 
 
-    public class DuplicateCheckBuilder<TModel>: IDuplicateCheckBuilder where TModel: class, IModel
+    public (Expression<Func<TModel,bool>> checker, Func<TModel, TModel, string> template) Build( TModel source )
     {
 
-        
-        public DuplicateCheckBuilder( Func<TModel,TModel,string> template, Func<TModel,Expression<Func<TModel,bool>>> predicate )
-        {
-
-            Template  = template;
-            Predicate = predicate;
-
-        }        
-        
-
-        private Func<TModel,TModel,string> Template { get; }
-        
-        private Func<TModel,Expression<Func<TModel,bool>>> Predicate { get; }
-
-
-        public (Expression<Func<TModel,bool>> checker, Func<TModel, TModel, string> template) Build( TModel source )
-        {
-
-            var expr    = PredicateBuilder.True<TModel>().And( Predicate(source) ).And( t=>t.Uid != source.Uid );
+        var expr    = PredicateBuilder.True<TModel>().And( Predicate(source) ).And( t=>t.Uid != source.Uid );
                 
-            // *****************************************************************                
-            return (expr, Template);
+        // *****************************************************************                
+        return (expr, Template);
 
-        }
-        
-        
     }
-    
-    
+        
+        
 }
